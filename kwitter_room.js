5b67791bb492825a202be1c2afa8830e5747dcaf
kpspiderman6 <73602349+kpspiderman6@users.noreply.github.com>
2020-12-16 17:49:28 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,22 @@
+
+//ADD YOUR FIREBASE LINKS HERE
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCBmLwMaB1klqrcWe2pcwvrhAmGb8fhuFQ",
+    authDomain: "kwitter-930c8.firebaseapp.com",
+    projectId: "kwitter-930c8",
+    storageBucket: "kwitter-930c8.appspot.com",
+    messagingSenderId: "823622710261",
+    appId: "1:823622710261:web:2f74ed539bcc17c708a6bc"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+
+      //End code
+      });});}
+getData();
